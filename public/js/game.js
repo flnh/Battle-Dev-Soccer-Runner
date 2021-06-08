2af1302edbc8615aa391af765c5e3c6546f4b36b
flnh <flnh57@gmail.com>
2021-06-08 15:39:04 +0200

    #6: Add assets

--- public/js/game.js
+++ public/js/game.js
@@ -2,8 +2,13 @@
 /// <reference path="../types/phaser.d.ts" />
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'container',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
     physics: {
         default: 'arcade',
         arcade: {
@@ -22,6 +27,7 @@ function preload() {
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     this.load.image('red', 'assets/particles/red.png');
     this.load.image('florian', 'https://media-exp3.licdn.com/dms/image/C4E03AQHvWhq-8o5crw/profile-displayphoto-shrink_800_800/0/1584003262799?e=1628726400&v=beta&t=G4jGW_pnbiv7NhaQWa0jar8RJasm-EsvMIbeqeWc7A4');
+    this.load;
 }
 function create() {
     this.add.image(400, 300, 'sky');
