9fd1738e3c53f8cea7abdd0697f4b40ff87aa532
AudeLsk <aude.lyskawa@easysav.com>
2021-06-09 11:00:27 +0200

    #5 Form target entity

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Target;
+use App\Form\TargetType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class GameController extends AbstractController
 {
@@ -15,6 +19,27 @@ class GameController extends AbstractController
     {
         return $this->render('game/index.html.twig', [
             'controller_name' => 'GameController',
+        ]);
+    }
+    /**
+     * @Route("/form", name="game")
+     */
+    public function form(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $target = new Target();
+        
+        $form = $this->createForm(TargetType::class, $target);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $target->setToken(md5(uniqid()));
+            $entityManager->persist($target);
+            $entityManager->flush();
+            // dump($target);
+        }
+
+        return $this->render('game/form.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
